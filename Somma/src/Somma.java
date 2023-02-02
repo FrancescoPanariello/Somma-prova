08d486441e48e2f505fad845581f56b518c86020
Giovannircc <72796439+Giovannircc@users.noreply.github.com>
2023-02-02 13:39:24 +0100

    Update Somma.java

--- Somma/src/Somma.java
+++ Somma/src/Somma.java
@@ -1,6 +1,12 @@
 public class Somma{
     public int sum(int a, int b){
-        return a2+b;
+        if(a==b)
+            return 2*a;
+        if(a==450*b)
+            return a/450
+        if(a==2.5)
+            return a
+        return a+b;
     }
     
      public int molt(int a, int b){
